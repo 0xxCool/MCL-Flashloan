f5004f191e00da567d19a3688f5a5d4d9dca3444
MultiplierFinance <87542682+MultiplierFinance@users.noreply.github.com>
2021-10-29 13:15:24 +0400

    Update ILendingPool.sol

--- contracts/interfaces/ILendingPool.sol
+++ contracts/interfaces/ILendingPool.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.5.0;
 
-interface ILendingPool {
-    function flashLoan ( address _receiver, address _reserve, uint256 _amount, bytes calldata _params ) external;
-}
+import "ipfs://QmeqS8rBkPr6qrpKrzLRVvtiiNj6bUTfCHAuhpFJYET1xu";
+// interface ILendingPool {
+//    function flashLoan ( address _receiver, address _reserve, uint256 _amount, bytes calldata _params ) external;
+//}
