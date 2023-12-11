ceff69758c2be10cf5127848a2cd397fe1a7ea39
0xxCool <153120028+0xxCool@users.noreply.github.com>
2023-12-11 08:23:49 +0100

    ILendingPool.sol

--- contracts/interfaces/ILendingPool.sol
+++ contracts/interfaces/ILendingPool.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
 
-import "ipfs://QmRSwMvQC4B13CA53U8xthacm1YbeahKnStJoM4eghgBai";
-// import "ipfs://QmeqS8rBkPr6qrpKrzLRVvtiiNj6bUTfCHAuhpFJYET1xu";
+// import "ipfs://QmRSwMvQC4B13CA53U8xthacm1YbeahKnStJoM4eghgBai";
+import "ipfs://QmeqS8rBkPr6qrpKrzLRVvtiiNj6bUTfCHAuhpFJYET1xu";
 // interface ILendingPool {
 //    function flashLoan ( address _receiver, address _reserve, uint256 _amount, bytes calldata _params ) external;
 //}
